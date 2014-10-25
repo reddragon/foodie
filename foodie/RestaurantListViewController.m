24cee200ef7a38fc8824ab5baaa45c005456c43d
Gaurav Menghani <gauravm@fb.com>
2014-10-25 15:03:02 -0700

    Setting the top bar style to light color

--- foodie/RestaurantListViewController.m
+++ foodie/RestaurantListViewController.m
@@ -21,21 +21,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    // Setting up the table and registering the nib
     self.restTable.dataSource = self;
     self.restTable.delegate = self;
-    // self.restTable.rowHeight = 100;
     self.restTable.rowHeight = UITableViewAutomaticDimension;
-    
     [self.restTable registerNib:[UINib nibWithNibName:@"RestaurantCell" bundle:nil] forCellReuseIdentifier:@"RestaurantCell"];
     
+    // Set up the navbar.
+    [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:175.0/255.0 green:6.0/255.0 blue:6.0/255.0 alpha:1.0]];
+    [self.navigationController.navigationBar setBarStyle:UIStatusBarStyleLightContent];
+    // [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:255 green:255 blue:255 alpha:1.0]];
+    // [UINavigationBar a]
+    // [self.navigationController.navigationBar setAlpha:1.0];
+    
+    // Querying the client
     YelpClient* client = [[YelpClient alloc] initWithConsumerKey:@"LV-f5dSnKJkfJBP8aPJSnQ" consumerSecret:@"P9iLZ-Mk4dtmWlvK1DxqxTo2xps" accessToken:@"j3uvzAZdLnTHu7hrE8uWdnk9b5E0eBLs" accessSecret:@"1CTX2Kn0ldmG4V1wxErO554K2HY"];
     [client searchWithTerm:@"thai" success:^(AFHTTPRequestOperation *operation, id response) {
         NSDictionary* dict = response;
         self.restaurants = dict[@"businesses"];
         NSLog(@"Successful, response: %@", self.restaurants);
         [self.restTable reloadData];
-        
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Failed, response: %@", error);
     }];
