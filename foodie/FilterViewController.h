42f4692c69f7d8e13c6a690599bd3cf453ad6eb1
Gaurav Menghani <gauravm@fb.com>
2014-10-25 23:41:11 -0700

    First step towards working on filters

---
+++ foodie/FilterViewController.h
@@ -0,0 +1,14 @@
+//
+//  FilterViewController.h
+//  foodie
+//
+//  Created by Gaurav Menghani on 10/25/14.
+//  Copyright (c) 2014 Gaurav Menghani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *filterTable;
+
+@end
